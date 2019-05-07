ff5cc36b62ffc74ecfa832e43ce41ae2755986bc
anaferreira015 <dasilvaferreira.af@hotmail.com>
2019-05-07 00:17:19 -0300

    Realiza ajustes na criptografia

--- encryption-of-caesar.c
+++ encryption-of-caesar.c
@@ -1,42 +1,122 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #define MAX_SIZE 256
 
-char *encryption(char *msg, int key) {
-	int size = strlen(msg), i;
-	for (i = 0; i < size; i++) {
-		msg[i] += key;
+#define ANSI_COLOR_RESET "\033[0;0m"
+#define ANSI_COLOR_LIGHT_MAGENTA "\033[1;95m"
+#define ANSI_COLOR_GREEN "\033[1;32m"
+typedef unsigned char UCHAR;
+
+UCHAR *encryption(UCHAR *msg, int key)
+{
+	int size = strlen(msg);
+	int i = 0, cont = 0;
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		while (i <= size)
+		{
+			if (msg[i] == letter)
+			{
+				msg[i] = letter + key;
+				i++;
+				cont++;
+				letter = 'a';
+			}
+			else if (msg[i] == ' ')
+			{
+				i++;
+				cont++;
+				letter = 'a';
+			}
+			else if (msg[i] == toupper(letter))
+			{
+				msg[i] = toupper(letter + key);
+				i++;
+				cont++;
+				letter = 'a';
+			}
+			else
+			{
+				letter++;
+			}
+			if (cont == size)
+				break;
+		}
 	}
 	return msg;
 }
 
-char *decryption(char *msg, int key) {
-	int size = strlen(msg), i;
-	for (i = 0; i < size; i++) {
-		msg[i] -= key;
+UCHAR *decryption(UCHAR *msg_cript, int key)
+{
+	int size = strlen(msg_cript);
+	int i = 0, cont = 0;
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		while (i <= size)
+		{
+			if (msg_cript[i] == letter)
+			{
+				msg_cript[i] = letter - key;
+				i++;
+				cont++;
+				letter = 'a';
+			}
+			else if (msg_cript[i] == ' ')
+			{
+				i++;
+				cont++;
+				letter = 'a';
+			}
+			else if (msg_cript[i] == toupper(letter))
+			{
+				msg_cript[i] = toupper(letter - key);
+				i++;
+				cont++;
+				letter = 'a';
+			}
+			else
+			{
+				letter++;
+			}
+			if (cont == size)
+				break;
+		}
 	}
-	return msg;
+	return msg_cript;
 }
 
-int main () {
+int main()
+{
 	int key, size, i;
-	char msg[MAX_SIZE], *msg_cript, *msg_decript;
-	
-	printf("Enter the key: \n");
-	scanf("%d", &key);
-	
+	UCHAR msg[MAX_SIZE], aux[MAX_SIZE], *msg_cript, *msg_decript;
+
 	printf("Enter the message: \n");
-	scanf("%[A-Z a-z,.]", msg);
-	
+	gets(msg);
+
+	printf("\nEnter the key: \n");
+	scanf("%d", &key);
+
 	msg_cript = encryption(msg, key);
-	
-	printf("\nCRYPTOGRAPHY MESSAGE: %s\n", msg_cript);
-	
+
+	printf(ANSI_COLOR_GREEN "\n===========================================");
+	printf(ANSI_COLOR_RESET);
+
+	printf(ANSI_COLOR_LIGHT_MAGENTA "\nCRYPTOGRAPHY MESSAGE: %s\n", msg_cript);
+
 	msg_decript = decryption(msg_cript, key);
-	
-	printf("\nMESSAGE DESCRIPTIONGRAPH: %s\n", msg_decript);
-	
+
+	printf(ANSI_COLOR_LIGHT_MAGENTA "\nMESSAGE DESCRIPTIONGRAPH: %s\n", msg_decript);
+	printf(ANSI_COLOR_RESET);
+
+	printf(ANSI_COLOR_GREEN "===========================================\n");
+	printf(ANSI_COLOR_RESET);
+
 	return 0;
 }
